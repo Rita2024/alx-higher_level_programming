7eaab4a549b4ae148e881bb08aaf7bd3e928592d
Rita2024 <ritaejezie2@gmail.com>
2023-06-14 18:45:28 -0700

    numbers of keys

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+
+    Ret_num = 0
+
+    list_keys = list(a_dictionary.keys())
+
+
+    for a in list_keys:
+
+        Ret_num += 1
+
+    return (Ret_num)
