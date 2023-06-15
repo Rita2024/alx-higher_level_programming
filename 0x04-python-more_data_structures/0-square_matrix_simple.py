08b5f7636a59493b2ca9b20409f46ecdf72ba47c
Rita2024 <ritaejezie2@gmail.com>
2023-06-14 18:28:45 -0700

    squared simple matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = matrix.copy()
+
+    for num in range(len(matrix)):
+        new_matrix[num] = list(map(lambda x: x**2, matrix[num]))
+
+    return (new_matrix)
