0b9116e571df21fd7a4b4aa403faf61c9025f0b5
Rita2024 <ritaejezie2@gmail.com>
2023-08-17 16:23:10 -0700

    updated

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
--- Query to create new user if not exist
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+-- Creates the user user_0d_1 with all privileges.
+CREATE USER 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
